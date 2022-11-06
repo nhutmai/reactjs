7ff858131600c3f19d84d53cc65a2863514d7451
vin mei <nhutmai00@gmail.com>
2022-11-07 02:10:53 +0800

    update 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          mai minh nhut
+          mai minh nhut bai hoc
         </p>
         <a
           className="App-link"
