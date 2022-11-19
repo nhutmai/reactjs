bdd1a7cada5fcc41950142c7b62b144d436f6146
vin mei <nhutmai00@gmail.com>
2022-11-20 01:46:11 +0800

    undate 7

--- src/features/album/index.jsx
+++ src/features/album/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Albumlist from './components/albumlist';
 
@@ -22,8 +22,12 @@ function Albumfeture(props) {
             link:'https://avatar-ex-swe.nixcdn.com/playlist/2022/11/14/2/0/5/1/1668415364486_300.jpg'
         },
     ];
+    const [count, setcount]= useState(0);
     return (
         <div>
+            {count}
+            <button onClick={() => setcount(x => x+1)}>tang</button>
+            <button onClick={() => setcount(0)}>reset</button>
             <p>co the ban se thich</p>
             <Albumlist albumlist={List}/>
         </div>
